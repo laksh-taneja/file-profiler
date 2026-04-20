da7bd08dd55de09d8f0102708921703d7e0cc894
Laksh <lakshtaneja1@gmail.com>
2026-04-20 15:13:09 +0530

    Refactor Analyze, use new file types and implement hash

--- internal/processor/engine.go
+++ internal/processor/engine.go
@@ -2,29 +2,58 @@ package processor
 
 import (
 	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-func Analyze(filename string, countLines bool) (int, error) {
-	log.Printf("Processing file %v", filename)
+func Analyze(f FileDimension) (*FileResult, error) {
+	log.Printf("Processing file %v", f.Filename)
 
-	file, err := os.Open(filename)
+	file, err := os.Open(f.Filename)
 	if err != nil {
-		return -1, err
+		return nil, err
 	}
 	defer file.Close()
 
-	var count int
+	var words int
+	var lines int
+	var checksum string
 
-	scanner := bufio.NewScanner(file)
-	if !countLines {
-		scanner.Split(bufio.ScanWords)
+	hasher := sha256.New()
+	var reader io.Reader = file
+
+	if f.DoHash {
+		reader = io.TeeReader(file, hasher)
+	}
+	if f.CountLines || f.CountWords {
+		scanner := bufio.NewScanner(reader)
+		if f.LongLines {
+			const maxCapacity = 10 * 1024 * 1024
+			buf := make([]byte, 1024*1024)
+			scanner.Buffer(buf, maxCapacity)
+		}
+		for scanner.Scan() {
+			if f.CountLines {
+				lines++
+			}
+			if f.CountWords {
+				lineText := scanner.Text()
+				words += len(strings.Fields(lineText))
+			}
+		}
+		if err = scanner.Err(); err != nil {
+			return nil, err
+		}
+	} else if f.DoHash {
+		io.Copy(io.Discard, reader)
+	}
+	if f.DoHash {
+		checksum = hex.EncodeToString(hasher.Sum(nil))
 	}
 
-	for scanner.Scan() {
-		count++
-	}
-
-	return count, nil
+	return &FileResult{WordCount: words, LineCount: lines, CheckSum: checksum}, nil
 }
