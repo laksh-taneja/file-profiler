ae271ac0db42a9661cd08b356c8181ba9e638227
Laksh <lakshtaneja1@gmail.com>
2026-04-21 19:25:23 +0530

    Add StartPool(workerCount) dimension, result worker pool

---
+++ internal/processor/worker.go
@@ -0,0 +1,31 @@
+package processor
+
+import (
+	"log"
+	"sync"
+)
+
+func StartPool(workerCount int) (chan FileDimension, chan *FileResult) {
+	jobs := make(chan FileDimension)
+	results := make(chan *FileResult)
+	var wg sync.WaitGroup
+
+	for range workerCount {
+		wg.Go(func() {
+			for job := range jobs {
+				res, err := Analyze(job)
+				if err != nil {
+					log.Printf("Error %v: %v\n", res.Filename, err)
+					continue
+				}
+				results <- res
+			}
+		})
+	}
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	return jobs, results
+}
